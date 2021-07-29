c9fb74013274bea2ff6a91700ce4dd62b5b4c5e1
PaTara43 <p040399@outlook.com>
2021-07-29 15:37:05 +0300

    Removed NFC reader usage, now it's income tracker! Adjusted robonomics toolkit usage Updated readme

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import rpi_funcs as rpi
-import io_funcs
 import sys
 import typing as tp
 import logging
 
 from classes import CoffeeMachine
-from robonomics_daos_toolkit import acl, action_logger, substrate_connection as subcon
+from robonomics_daos_toolkit import acl, action_logger, obtain_incomes, common_utils as cu
 
 # set up logging
 logging.basicConfig(
@@ -15,74 +13,66 @@ logging.basicConfig(
 )
 
 # load up configuration
-config: tp.Dict[str, tp.Any] = io_funcs.read_config()
-if not config:
-    logging.critical("Config load error, exiting")
-    sys.exit()
-else:
-    logging.info(f"Config load successful\n{config}")
+config: tp.Dict[str, tp.Any] = cu.read_yaml_file("config.yaml")
 
-# initializing all daos tools
-
-if config["use_daos_toolkit"]:
-    substrate = subcon.substrate_connection(config["daos_toolkit"]["substrate"])
-    if config["daos_toolkit"]["use_acl"]:
-        # initiate an instance of ACL class based on digital twin of a device
-        logging.info("initiating ACL class")
-        acl_obj = acl.ACL(config['daos_toolkit'], substrate)
-        if not acl_obj:
-            logging.critical("ACL instance error, exiting")
-            sys.exit()
-        else:
-            logging.info("ACL initiated")
-    if config["daos_toolkit"]["use_action_logger"]:
-        # initiate an instance of action logger
-        logging.info("initiating ActionLogger class")
-        action_logger = action_logger.ActionLogger(config['daos_toolkit'], substrate)
-        if not action_logger:
-            logging.critical("ActionLogger instance error, exiting")
-            sys.exit()
-        else:
-            logging.info("ActionLogger initiated")
+# connect to substrate
+substrate = cu.substrate_connection(config["daos_toolkit"]["substrate"])
 
 # initialize an instance of CoffeeMachine object
 coffee_machine = CoffeeMachine(
     gpio_outputs=[0, 21, 0, 0, 0, 0, 0]
 )
 
-# entry point
-if __name__ == "__main__":
-    logging.info("Daemon started")
 
-    # start the daemon
-    while True:
+# initializing all daos tools
+if config["use_daos_toolkit"]:
 
-        # poll NFC reader until input received
-        logging.info("Start polling NFC reader")
-        while True:
-            tag_id: str = rpi.poll_nfc_reader()
-            if tag_id:
-                logging.info(f"Found NFC tag with UID {tag_id}. Stopped polling")
-                break
+    if config["daos_toolkit"]["use_acl"]:
+        # initiate an instance of ACL class based on digital twin of a device
+        acl_obj = acl.ACL(config["daos_toolkit"], cu.substrate_connection(config["daos_toolkit"]["substrate"]))
+        if not acl_obj:
+            sys.exit()
 
-        if config["daos_toolkit"]["use_acl"]:
-            # check if user is allowed to use the machine
-            if not acl_obj.usage_allowed(tag_id):
-                logging.warning(f"UID {tag_id} is not in the allow list. Usage declined.")
-                continue
-            else:
-                logging.info(f"UID {tag_id} allowed. Continuing.")
+    if config["daos_toolkit"]["use_action_logger"]:
+        # initiate an instance of action logger
+        action_logger = action_logger.ActionLogger(config["daos_toolkit"],
+                                                   cu.substrate_connection(config["daos_toolkit"]["substrate"]))
+        if not action_logger:
+            sys.exit()
 
-        # make a coffee if usage allowed
-        operation = coffee_machine.make_a_coffee()
+logging.info("Coffee machine daemon started")
 
-        if operation["success"]:
-            logging.info("Operation successful.")
-            if config["daos_toolkit"]["use_action_logger"]:
-                action_logger.log_action(f"Making coffee for {tag_id}", "success")
+# start the daemon
+income_tracker = obtain_incomes.IncomeTracker(config["daos_toolkit"], substrate)
+
+while True:
+    # wait for money income event
+    income_tracker.money_income_event.wait()
+    income_tracker.money_income_event.clear()
+
+    if config["daos_toolkit"]["use_acl"]:
+        # check if user is allowed to use the machine
+        if not acl_obj.usage_allowed(income_tracker.money_income_event.source_address):
+            logging.warning(f"Account {income_tracker.money_income_event.source_address} is not in the allow list. "
+                            f"Usage declined. Thanks for the money")
+            continue
         else:
-            logging.error(f"Operation FAILED: {operation['message']}")
-            if config["daos_toolkit"]["use_action_logger"]:
-                action_logger.log_action(f"Making coffee for {tag_id}", "failure")
+            logging.info(f"Account {income_tracker.money_income_event.source_address} allowed. Continuing.")
 
-        logging.info("Session over")
+    # Coffee making part
+    operation = coffee_machine.make_a_coffee()
+
+    if operation["success"]:
+
+        logging.info("Operation successful.")
+        if config["daos_toolkit"]["use_action_logger"]:
+            action_logger.log_action(f"Making coffee for {income_tracker.money_income_event.source_address}.",
+                                     "success")
+    else:
+
+        logging.error(f"Operation FAILED.")
+        if config["daos_toolkit"]["use_action_logger"]:
+            action_logger.log_action(f"Making coffee for {income_tracker.money_income_event.source_address}.",
+                                     "FAILURE: " + operation["message"])
+
+    logging.info("Session over")
--- rpi_funcs.py
+++ rpi_funcs.py
@@ -1,5 +1,4 @@
 # functions which refer to hardware operation of a host machine (Raspberry Pi)
-import subprocess
 from gpiozero import LED
 from time import sleep
 import logging
@@ -10,23 +9,6 @@ logging.basicConfig(
     filename="daemon.log",
     format="%(asctime)s %(levelname)s: %(message)s"
 )
-
-
-def poll_nfc_reader() -> str:
-    """
-    polls nfc reader using libnfc cli interface.
-    polling has to be reinitialized every 36 seconds.
-
-    :return: UID of an NFC tag if found one
-    """
-
-    command = "sudo nfc-poll | grep UID | cut -d : -f 2"
-    output = subprocess.getoutput(command)
-    if output:
-        tag_id = output.split("\n")[1].strip()
-        return tag_id
-    else:
-        return ""
 
 
 def trigger_transistor(channel: int = 18) -> None:
